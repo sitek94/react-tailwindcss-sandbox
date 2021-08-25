25e4a36ef109c8dab6330f6a6b60355d5935406c
Maciek Sitkowski <maciej.sitkowski@netguru.com>
2021-08-25 21:13:17 +0200

    refactor: create examples dir

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import * as ReactDOM from 'react-dom';
 import './index.css';
 
 const defaultAppPath = 'Home';
+const examplesDir = 'examples';
 
 // This is the argument that you pass when running `npm start --app=<app_path>`
 const appPath = process.env.REACT_APP_APP_PATH || defaultAppPath;
 
 // The component is lazy loaded, so that it is possible to dynamically construct
 // a path to the app's directory
-const App = React.lazy(() => import(`./${appPath}`));
+const App = React.lazy(() => import(`./${examplesDir}/${appPath}`));
 
 ReactDOM.render(
   <React.StrictMode>
