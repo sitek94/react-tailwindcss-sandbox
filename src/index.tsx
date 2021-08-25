34f349e8947facef294be0ffaa8cbe5fb5055d22
Maciek Sitkowski <maciej.sitkowski@netguru.com>
2021-08-25 21:29:21 +0200

    feat: add check for userSpecifiedAppPath

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,14 @@ import './index.css';
 
 const defaultAppPath = 'Home';
 const examplesDir = 'examples';
+const userSpecifiedAppPath = process.env.REACT_APP_APP_PATH;
+
+if (!userSpecifiedAppPath) {
+  console.log(`⚠️ "app_path" was not found, running default app.`);
+}
 
 // This is the argument that you pass when running `npm start --app=<app_path>`
-const appPath = process.env.REACT_APP_APP_PATH || defaultAppPath;
+const appPath = userSpecifiedAppPath || defaultAppPath;
 
 // The component is lazy loaded, so that it is possible to dynamically construct
 // a path to the app's directory
