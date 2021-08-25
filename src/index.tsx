a3efde46f37df9c80a47cd2aab5c65dc6cb9c3bc
Maciek Sitkowski <maciej.sitkowski@netguru.com>
2021-08-25 14:46:46 +0200

    feat: init prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import "./index.css";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './index.css';
 
-import App from "./App";
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
